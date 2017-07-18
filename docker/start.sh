d18f85bd4cd03dac0ad01e50612500719a9ba9fd
Docker Builder <docker-builder@getshine.com>
2017-07-18 12:28:19 +0300

    simplify start script

--- docker/start.sh
+++ docker/start.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 # Script to run a java application for testing jmx4prometheus.
-env
 
 RUNDIR=${RUNDIR:-/usr/local/jmx_exporter}
 echo RUNDIR $RUNDIR
+
 BIND_HOST=${BIND_HOST:-localhost}
-echo BINDING TO HOST:PORT $BIND_HOST:39390
-CONFIG_FILE_DIR=${CONFIG_FILE_DIR:-/usr/local/jmx_exporter/jmx_prometheus_httpserver/config/config.yml}
+BIND_PORT=${BIND_PORT:-39390}
+BIND_HOST_PORT=$BIND_HOST:$BIND_PORT
+echo BINDING TO HOST:PORT $BIND_HOST_PORT
+
+CONFIG_FILE_DIR=${CONFIG_FILE_DIR:-/tmp/jmx_exporter.yml}
 echo GOING TO USE $CONFIG_FILE_DIR AS INPUT CONFIG 
+echo THERFORE IT NEEDS TO BE MOUNTED FROM THE HOST (-v YOUR_PATH/your_config.yml:/tmp/jmx_exporter.yml)
+
 VERSION=$JMX_PROMETHEUS_VERSION
 echo using jar jmx_prometheus_httpserver-${VERSION}-jar-with-dependencies.jar
-java -jar  $RUNDIR/jmx_prometheus_httpserver/target/jmx_prometheus_httpserver-${VERSION}-jar-with-dependencies.jar $BIND_HOST $CONFIG_FILE_DIR
+
+java -jar  $RUNDIR/jmx_prometheus_httpserver/target/jmx_prometheus_httpserver-${VERSION}-jar-with-dependencies.jar $BIND_HOST_PORT $CONFIG_FILE_DIR
 
